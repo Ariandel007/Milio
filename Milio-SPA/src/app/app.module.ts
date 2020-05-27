f2cc87a0c94ea49587ff07bffdbd68ad4bd8207b
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2020-05-27 10:18:19 -0500

    footer added

--- Milio-SPA/src/app/app.module.ts
+++ Milio-SPA/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HeroComponent } from './hero/hero.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel'
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
-      HeroComponent
+      HeroComponent,
+      FooterComponent
    ],
    imports: [
       BrowserModule,
