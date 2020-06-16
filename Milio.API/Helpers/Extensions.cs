e74ede14e14364d533a39fd14df51088c7adf025
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2020-06-16 17:00:39 -0500

    json, pascal to camel for pagination

--- Milio.API/Helpers/Extensions.cs
+++ Milio.API/Helpers/Extensions.cs
@@ -1,9 +1,33 @@
 using System;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Milio.API.Helpers
 {
     public static class Extensions
     {
+        public static void AddAplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+        
+        public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            //haremos una onversion de pascal case a camel case porque Angular todo
+            //lo trata como camelCase
+            var camelCaseFormatter = new JsonSerializerSettings();
+            camelCaseFormatter.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormatter));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+
+        }
+
         public static int CalculateAge(this DateTime theDateTime)
         {
             var age = DateTime.Today.Year - theDateTime.Year;
@@ -13,5 +37,7 @@ namespace Milio.API.Helpers
             
             return age;
         }
+
     }
+
 }
