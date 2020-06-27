056e69b57fa0927a82f220e81f57ce4ec8854fab
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2020-06-27 00:50:44 -0500

    project test added

---
+++ Milio.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Milio.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
