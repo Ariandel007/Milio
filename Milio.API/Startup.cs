542069fe32a4f139fc015515758944501fce23f5
Alexander Gonzalo Urbina Mar <43892122+Ariandel007@users.noreply.github.com>
2020-07-01 11:14:32 -0500

    thread pool

--- Milio.API/Startup.cs
+++ Milio.API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -43,6 +44,8 @@ namespace Milio.API
                 opt.Password.RequireNonAlphanumeric = false;
                 opt.Password.RequireUppercase = false;
             });
+            services.Configure<KestrelServerOptions>(
+                Configuration.GetSection("Kestrel"));
 
             builder = new IdentityBuilder(builder.UserType, typeof(Role), builder.Services);
             builder.AddEntityFrameworkStores<DataContext>();
